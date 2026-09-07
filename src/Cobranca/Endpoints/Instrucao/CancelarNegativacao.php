87c7c4de4f02d016da220152737888b20f6d296b
Vinícius de Senna <sennadevinicius@gmail.com>
2026-09-07 13:32:06 -0300

    Ajuste use

--- src/Cobranca/Endpoints/Instrucao/CancelarNegativacao.php
+++ src/Cobranca/Endpoints/Instrucao/CancelarNegativacao.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ailos\Sdk\Cobranca\Endpoints\Instrucao;
 
-use use Ailos\Sdk\Cobranca\Endpoints\Endpoint;
+use Ailos\Sdk\Cobranca\Endpoints\Endpoint;
 use Ailos\Sdk\Http\Request;
 
 /**
